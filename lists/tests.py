a8fb517d8aef7c22d0ba3d58bd169c1ee0a1ecb8
Le Xiao <xiaole@atrihub.io>
2017-07-28 14:49:08 -0700

    refactor(LX TDD Training): Change Test
    Modified the content of test
    
    ICTRAINING-22

--- lists/tests.py
+++ lists/tests.py
@@ -18,6 +18,11 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
     def test_home_page_return_correct_html(self):
         request = HttpRequest()
         # response = home_page(request)
