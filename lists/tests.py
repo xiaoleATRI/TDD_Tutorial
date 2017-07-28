c28ec6cf5dbe3e1f1662d369907f0ac839d388a6
Le Xiao <xiaole@atrihub.io>
2017-07-28 11:56:52 -0700

    refactor(LX TDD Training): Refactor Home page
    Refactor the home page view with the template
    
    ICTRAINING-21

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 # Create your tests here.
 
 
@@ -19,8 +20,12 @@ class HomePageTest(TestCase):
 
     def test_home_page_return_correct_html(self):
         request = HttpRequest()
-        response = home_page(request)
+        # response = home_page(request)
+        response = self.client.get('/')
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
+        '''expected_html = render_to_string('home.html')
+        self.assertEqual(html, expected_html)'''
+        self.assertTemplateUsed(response, 'home.html')
--- lists/views.py
+++ lists/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    return HttpResponse('<html><title>To-do lists</title></html>')
+    return render(request, 'home.html')
