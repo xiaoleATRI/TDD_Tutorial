a59c04bf80ced38ceaff1bd4ad2a0279a6714bd3
Le Xiao <xiaole@atrihub.io>
2017-08-01 15:12:35 -0700

    feat(LX TDD Training): Deployment on Heroku
    Configuration made for heroku
    
    ICTRAINING-26

--- superlists/settings.py
+++ superlists/settings.py
@@ -140,3 +140,8 @@ STATIC_URL = '/static/'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
