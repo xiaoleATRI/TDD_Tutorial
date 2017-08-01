3b320a0558ff625736af83e4b599d00a870e53d7
Le Xiao <xiaole@atrihub.io>
2017-08-01 14:27:40 -0700

    fix(LX TDD Training): Collectstatic
    Configured STATIC_ROOT
    
    ICTRAINING-25

--- superlists/settings.py
+++ superlists/settings.py
@@ -14,7 +14,7 @@ import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -129,13 +129,13 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.abspath(os.path.join(PROJECT_ROOT, '../static'))
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
-]
+'''STATICFILES_DIRS = [
+    os.path.abspath(os.path.join(PROJECT_ROOT, '../static'))
+]'''
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
