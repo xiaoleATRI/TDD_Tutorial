f12ea41e5d550bedf9f64bc2311ff147cec70075
Le Xiao <xiaole@atrihub.io>
2017-08-03 12:37:53 -0700

    refactor(LX TDD Training):Model Validation Enforcement
    Enforce model validation on list view
    
    ICTRAINING-30

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -17,7 +17,7 @@ class ItemValidationTest(FunctionalTest):
         # that list items cannot be blank
         # wait for loading of page
         self.wait_for(lambda: self.assertEqual(
-                  self.browser.find_element_by_css_selector('.has_error').text,
+                  self.browser.find_element_by_css_selector('.has-error').text,
                   "You can't have an empty list item"
               )
         )
@@ -30,7 +30,7 @@ class ItemValidationTest(FunctionalTest):
         self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
         # She receives a similar warning on the list page
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has_error').text,
+            self.browser.find_element_by_css_selector('.has-error').text,
             "You can't have an empty list item"
         )
                       )
