a6c035bdd979bd33c931daa698699c66721789ad
Le Xiao <xiaole@atrihub.io>
2017-08-02 17:00:44 -0700

    fix(LX TDD Training): Test Heroku
    Different setting for test
    
    ICTRAINING-26

--- superlists/settings.py
+++ superlists/settings.py
@@ -122,7 +122,7 @@ TEST_DATABASES = {
 }
 
 # add test running class
-# TEST_RUNNER = 'config.test_suite.runner.HerokuTestSuiteRunner'
+TEST_RUNNER = 'test_suite_runner.HerokuTestSuiteRunner'
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
