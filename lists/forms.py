bcf71d24959f166249b535af4a9e7f5d7edf236f
Le Xiao <xiaole@atrihub.io>
2017-08-03 16:17:08 -0700

    refactor(LX TDD Training): Use Form
    Use form into all views
    
    ICTRAINING-31

--- lists/forms.py
+++ lists/forms.py
@@ -12,7 +12,7 @@ class ItemForm(forms.models.ModelForm):
         fields = ('text',)
         widgets = {
             'text': forms.fields.TextInput(attrs={
-                'placeholder':'Enter a to-do item',
+                'placeholder': 'Enter a to-do item',
                 'class': 'form-control input-lg',
             })
         }
