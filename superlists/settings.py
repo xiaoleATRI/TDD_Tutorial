77d73088b0c371bdf38a91c66a90dbba335e43c1
Le Xiao <xiaole@atrihub.io>
2017-08-02 17:18:42 -0700

    fix(LX TDD Training): Ready to create staging branch
    Created staging branch
    
    ICTRAINING-26

--- superlists/settings.py
+++ superlists/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 import dj_database_url
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,7 +121,7 @@ TEST_DATABASES = {
 }
 
 # add test running class
-TEST_RUNNER = 'test_suite_runner.HerokuTestSuiteRunner'
+# TEST_RUNNER = 'test_suite_runner.HerokuTestSuiteRunner'
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
