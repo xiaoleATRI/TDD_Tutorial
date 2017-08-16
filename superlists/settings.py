8162c2bbf1f3ce4809c09caba495b9dc7869426c
Le Xiao <xiaole@atrihub.io>
2017-08-16 14:13:36 -0700

    feat(LX TDD Training): BDD
    Integrated with BDD
    
    ICTRAINING-26

--- superlists/settings.py
+++ superlists/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
-    'lists'
+    'lists',
+    'functional_tests',
+    'behave_django'
 ]
 
 MIDDLEWARE = [
